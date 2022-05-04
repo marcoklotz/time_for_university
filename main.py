2854bfa7c2f0df4daa2e274dedcf403963f82436
marcoklotz <marco_klotz@gmx.de>
2022-05-04 11:59:51 +0200

    rearranged code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def time_for_university():
-    ects_sum = int(input("How many ECTS do you want to gain this semester? "))
+    ects_sum = 0
+    ects_sum = int(input("How many ECTS do you want to gain this semester?: "))
     for i in range(0, ects_sum):
-
         if ects_sum < 30:
             print("This degree might take you longer than planed! ")
         elif ects_sum == 30:
@@ -12,10 +12,8 @@ def time_for_university():
 def total_hours_per_week():
     time_per_ects = 30  # amount of hours per ECTS
     weeks_per_semester = 13
-    total_time_spent = float
-    total_time_spent = (ects_sum * time_per_ects)/weeks_per_semester
-
-    print("You have to put in" + total_time_spent + "hours per week", end='' )
+    total_time_spent = (ects_sum * 30)/13
+    print("You have to put in {total_time_spent} hours per week", end='' )
 
 if __name__== "__time_for_university__":
     time_for_university()
