87b1ca939a23c0ef444658ed5d9ca4615c341f13
marcoklotz <marco_klotz@gmx.de>
2022-05-06 12:14:37 +0200

    trying to solve why input is not working

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 def time_for_university():
-    ects_sum = 0
-    ects_sum = int(input("How many ECTS do you want to gain this semester?: "))
-    for i in range(0, ects_sum):
-        if ects_sum < 30:
-            print("This degree might take you longer than planed! ")
-        elif ects_sum == 30:
-            print("You are right on track to finish your studies on time! ")
-        else:
-            print("Very good! ")
+    ects_sum = input('How many ECTS do you want to gain this semester: ')
+    #for i in range(0, ects_sum):
+    #    if ects_sum < 30:
+    #        print("This degree might take you longer than planed! ")
+    #    elif ects_sum == 30:
+    #        print("You are right on track to finish your studies on time! ")
+    #    else:
+    #        print("Very good! ")
 
-def total_hours_per_week():
-    time_per_ects = 30  # amount of hours per ECTS
-    weeks_per_semester = 13
-    total_time_spent = (ects_sum * 30)/13
-    print("You have to put in {total_time_spent} hours per week", end='' )
-
-if __name__== "__time_for_university__":
-    time_for_university()
+#def total_hours_per_week():
+   # time_per_ects = 30  # amount of hours per ECTS
+   # weeks_per_semester = 13
+  #  total_time_spent = (ects_sum * 30)/13
+ #   print("You have to put in {total_time_spent} hours per week", end='' )
